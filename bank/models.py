cb808ee74a566eb6502d472ab64ca9b874f2a0c5
amaduesSCARLET <3380464659@qq.com>
2022-07-05 00:05:38 +0800

    增加了注释

--- bank/models.py
+++ bank/models.py
@@ -1,55 +1,68 @@
 from django.db import models
 
 # Create your models here.
+# 信用卡额度分级表(credit——>limit)
 class creditCard_type(models.Model):
     credit=models.IntegerField(primary_key=True)
     limit=models.FloatField()
 
+# 顾客表(id——>name,id——>customer_credit)
 class customer(models.Model):
     name=models.CharField(max_length=32)
     customer_credit=models.ForeignKey(to=creditCard_type,to_field="credit",on_delete=models.CASCADE)
 
+# 顾客账户表(id——>key,id——>balance,id——>account_customer)
 class account(models.Model):
     key=models.CharField(max_length=60)
     balance=models.FloatField()
     account_customer=models.ForeignKey(to=customer,to_field="id",on_delete=models.CASCADE)
 
+# 信用卡表(id——>creditcard_account,id——>limit_remain)
 class credit_card(models.Model):
     creditcard_account = models.ForeignKey(to=account, to_field="id", on_delete=models.CASCADE)
     limit_remain=models.FloatField()
 
-
+# 银行表(id——>name,id——>rate)
 class bank(models.Model):
     name=models.CharField(max_length=32)
     rate=models.FloatField(default=0.045)
 
+# 交易类型表（转账，取现，存款）(TypeId——>Type)
 class transactionType_list(models.Model):
     TypeId=models.IntegerField(primary_key=True)
     Type=models.CharField(max_length=20)
 
+# 消费类型表（教育，交通，购物等）(CoTypeId——>Type)
 class consumptionType_list(models.Model):
     CoTypeId = models.IntegerField(primary_key=True)
     Type=models.CharField(max_length=20)
 
+# 交易记录表(id——>amount,id——>time,id——>useCreditCard,
+# id——>inAccount,id——>outAccount,id——>transactionType,id——>consumptionType)
 class transaction_history(models.Model):
     amount=models.FloatField()
     time=models.DateField()
     useCreditCard=models.BooleanField(default=False)
-    inAccount = models.ForeignKey(to=account, to_field="id",null=True,on_delete=models.CASCADE,related_name='inAccount',default=None)
-    outAccount = models.ForeignKey(to=account, to_field="id",null=True,on_delete=models.CASCADE,related_name='outAccount',default=None)
+    inAccount = models.ForeignKey(to=account, to_field="id",null=True,on_delete=models.CASCADE,
+                                  related_name='inAccount',default=None)
+    outAccount = models.ForeignKey(to=account, to_field="id",null=True,on_delete=models.CASCADE,
+                                   related_name='outAccount',default=None)
     transactionType=models.ForeignKey(to=transactionType_list,to_field="TypeId",on_delete=models.CASCADE)
     consumptionType=models.ForeignKey(to=consumptionType_list,to_field="CoTypeId",on_delete=models.CASCADE)
 
+# 信用卡流水类型表（消费，还款）(CeTypeId——>Type)
 class creditCardhistoryType_list(models.Model):
     CeTypeId = models.IntegerField(primary_key=True)
     Type=models.CharField(max_length=20)
 
+# 信用卡交易记录表(id——>time,id——>amount,id——>creditCardHistory_account,id——>creditCardhistoryType)
 class creditCard_history(models.Model):
     time=models.DateField()
     amount=models.FloatField()
     creditCardHistory_account = models.ForeignKey(to=account, to_field="id", on_delete=models.CASCADE)
     creditCardhistoryType=models.ForeignKey(to=creditCardhistoryType_list,to_field="CeTypeId",on_delete=models.CASCADE)
 
+# 贷款表(id——>amount,id——>year_interest_rate,id——>time_limit,id——>time,id——>loan_customer)
 class loan(models.Model):
     amount=models.FloatField()
     year_interest_rate=models.FloatField(default=0.045)
@@ -57,6 +70,7 @@ class loan(models.Model):
     time=models.DateField()
     loan_customer=models.ForeignKey(to=customer,to_field="id",on_delete=models.CASCADE)
 
+# 还款表(id——>amount,id——>time,id——>repayment_loan,id——>repayment_customer)
 class repayment(models.Model):
     amount = models.FloatField()
     time = models.DateField()
--- script/script.py
+++ script/script.py
@@ -1,3 +1,7 @@
+"""""""""
+操作数据库的脚本函数，具体操作在代码末尾，可以分别执行
+"""""""""
+
 import datetime
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tree_frog_bank.settings")
@@ -6,7 +10,11 @@ from django.db.models import Sum
 django.setup()
 from bank.models import *
 
+
+#本次实现的类以及全部功能函数
 class treeFrogBank():
+
+
     def costomer_baseInformation(id):
         print(customer.objects.filter(id=id)[0].name,"你共开通了如下账户")
         for i in account.objects.filter(account_customer_id=id):
@@ -206,31 +214,106 @@ class treeFrogBank():
 
 
 
-
+# 查询顾客id为1的基本信息，如开通的账户
 # treeFrogBank.costomer_baseInformation(1)
+
+
+# 修改id为1的顾客的密码
 # treeFrogBank.customer_accountKEY_revise(1,123456,66666666)
 # treeFrogBank.customer_accountKEY_revise(1,"144030663","66666666")
+
+
+# 查询id为1的顾客的所有账户的余额
 # treeFrogBank.costomer_Inquire_idbalance(id=1)
+
+
+# 查询账户id为3的余额
 # treeFrogBank.costomer_Inquire_accountbalance(accountid=3)
+
+
+# 实现查询顾客的利率
 # treeFrogBank.costomer_Inquire_rate(1)
+
+
+# 实现查询顾客id为1的额度
 # treeFrogBank.costomer_Inquire_credit(1)
+
+
+# 对id为1的账户，在7月3日存入100元
 # treeFrogBank.costomer_inAccount(inaccountid=1,time=datetime.date(2022,7,3),amount=100)
+
+
+# 对id为1的账户，在7月3日，取出20元，其中不使用信用卡，消费类型编号为1（教育）
 # treeFrogBank.costomer_outAccount(outaccountid=1,time=datetime.date(2022,7,2),
 #                                  amount=20,useCreditCard=False,consumptionType=1)
+
+
+# 对id为1的账户，在7月3日，取出10元，使用信用卡，消费类型编号为1（教育）
 # treeFrogBank.costomer_outAccount(outaccountid=1,time=datetime.date(2022,7,2),
 #                                  amount=10,useCreditCard=True,consumptionType=1)
+
+
+# 账户1向账户2于7月2号转入10元，账户1未使用信用卡，消费类型为2（交通）
 # treeFrogBank.costomer_transAccount(
 #     inaccountid=2,outaccountid=1,time=datetime.date(2022,7,2),
 #     amount=10,useCreditCard=False,consumptionType=2)
+
+
+# 账户1向账户2于7月2号转入20元，账户1使用了信用卡，消费类型为2（交通）
 # treeFrogBank.costomer_transAccount(
 #     inaccountid=2,outaccountid=1,time=datetime.date(2022,7,2),
 #     amount=20,useCreditCard=True,consumptionType=2)
+
+
+# 在每月4号还款
 # treeFrogBank.creditCardIN(4)
+
+
+# 在每月3号还款
 # treeFrogBank.creditCardIN(3)
+
+
+# 查询顾客id为1的客户，使用多个账户在7月3日的消费
 # treeFrogBank.tradeInquire(1,time=datetime.date(2022,7,3))
+
+
+# 查询顾客id为1的客户，使用多个账户在7月2日的消费
 # treeFrogBank.tradeInquire(1,time=datetime.date(2022,7,2))
-# treeFrogBank.tradeAnalyse(1,2022,7)
+
+
+# 查询id为1的顾客2022年7月消费分布
+# treeFrogBank.tradeAnalyse(6,2022,7)
+
+# Id为1的顾客在7月3号贷款1000000000元
 # treeFrogBank.customer_loan(1000000000,25,datetime.date(2022,7,3),1)
+
+
+# Id为1的顾客在7月3号贷款1000元
 # treeFrogBank.customer_loan(1000,25,datetime.date(2022,7,3),1)
+# Id为1的顾客在2023年对借的单号5的贷款还款81.175元
 # treeFrogBank.customer_repayment(amount=81.175,time=datetime.date(2025,7,3),id=1,loanid=5)
+
+
+# 查询单号为5的还款情况
 # treeFrogBank.customerRepaymentInquire(5)
+
+
+# 还款量单独查询（请在sql console中执行）
+# select sum(bank_repayment.amount) as "还款总量",bank_loan.amount-sum(bank_repayment.amount) as "待还清"
+# from  bank_repayment,bank_loan
+# where repayment_loan_id=5
+#   and bank_loan.id=bank_repayment.repayment_loan_id;
+
+# 10题中测试索引
+# import platform
+# # 计时器
+# print('系统:',platform.system())
+#
+# import time
+# T1 = time.perf_counter()
+#
+# #______程序部分______
+# treeFrogBank.customerRepaymentInquire(5)
+#
+# T2 =time.perf_counter()
+# print('程序运行时间:%s毫秒' % ((T2 - T1)*1000))
